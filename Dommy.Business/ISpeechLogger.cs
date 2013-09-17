94a550a497035e7d99446bd8ade8d25dc29cf720
Aurélien GALTIER <aurelien.galtier@live.fr>
2013-09-17 20:32:09 +0200

    Optimize AsyncHelper and add unit test

---
+++ Dommy.Business/ISpeechLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dommy.Business
+{
+    public interface ISpeechLogger
+    {
+        void ErrorRecognition(Actor actor, string p);
+
+        void Say(Actor actor, string p);
+    }
+}
