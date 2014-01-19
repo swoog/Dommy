e68b5d3f4b39ba2b06a886155b4f8275ff8b7318
Aurelien GALTIER <aurelien.galtier@live.fr>
2014-01-19 20:29:34 +0100

    Add KinectSkeletonListener

---
+++ Dommy.Extensions.Kinect/KinectSkeletonListener.cs
@@ -0,0 +1,56 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="KinectSkeletonListener.cs" company="TrollCorp">
+//     Copyright (c) agaltier, TrollCorp. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Dommy.Extensions.Kinect
+{
+    using Dommy.Business;
+    using Microsoft.Kinect;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public sealed class KinectSkeletonListener : IListener
+    {
+        /// <summary>
+        /// Instance of the kinect sensor.
+        /// </summary>
+        private KinectSensor kinect = null;
+
+        public void Init(Engine engine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        //    using (var skeletonFrame = this.kinect.SkeletonStream.OpenNextFrame(0))
+        //    {
+        //        // Sometimes we get a null frame back if no data is ready
+        //        if (null == skeletonFrame)
+        //        {
+        //            return;
+        //        }
+
+        //        // Reallocate if necessary
+        //        if (null == skeletonData || skeletonData.Length != skeletonFrame.SkeletonArrayLength)
+        //        {
+        //            skeletonData = new Skeleton[skeletonFrame.SkeletonArrayLength];
+        //        }
+
+        //        skeletonFrame.CopySkeletonDataTo(skeletonData);
+        //        skeletonDrawn = false;
+        //    }
+        //}
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
