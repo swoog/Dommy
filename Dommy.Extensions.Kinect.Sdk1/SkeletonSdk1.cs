fef540b0d3286e2f07599bbcd5705ea63c35d094
Aurelien GALTIER <aurelien.galtier@live.fr>
2014-02-22 22:45:34 +0100

    Add skeleton definition sdk1

---
+++ Dommy.Extensions.Kinect.Sdk1/SkeletonSdk1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dommy.Extensions.Kinect.Sdk1
+{
+    public class SkeletonSdk1 : ISkeleton
+    {
+        private Microsoft.Kinect.Skeleton skeleton;
+
+        public SkeletonSdk1(Microsoft.Kinect.Skeleton skeleton)
+        {
+            // TODO: Complete member initialization
+            this.skeleton = skeleton;
+        }
+        public string TrackingId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IList<IJointType> GetJointTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IJointType this[IJointType joint]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
