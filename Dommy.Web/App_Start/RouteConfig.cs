4c61e063b1078a4efd417e505d6aa3ada65bab1e
Aurelien GALTIER <aurelien.galtier@live.fr>
2014-03-16 09:59:38 +0100

    Add light on off images

--- Dommy.Web/App_Start/RouteConfig.cs
+++ Dommy.Web/App_Start/RouteConfig.cs
@@ -15,6 +15,12 @@ namespace Dommy.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Resource",
+                url: "resource/{resourceName}",
+                defaults: new { controller = "Resource", action = "Index"}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Dommy.Web/Controllers/ResourceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dommy.Web.Controllers
+{
+    public class ResourceController : Controller
+    {
+        //
+        // GET: /Resource/
+        public ActionResult Index(string resourceName)
+        {
+            var assembly = typeof(Dommy.Business.Engine).Assembly;
+
+            Stream stream = assembly.GetManifestResourceStream(resourceName);
+            return this.File(stream, GetContentType(resourceName));
+        }
+
+        private static string GetContentType(string resourceName)
+        {
+            var extention = resourceName.Substring(resourceName.LastIndexOf('.')).ToLower();
+            switch (extention)
+            {
+                case ".gif":
+                    return "image/gif";
+                case ".js":
+                    return "text/javascript";
+                case ".css":
+                    return "text/css";
+                default:
+                    return "text/html";
+            }
+        }
+
+    }
+}
