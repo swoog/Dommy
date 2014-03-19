1e6ec3d027a9040321984cb358f89044d1210e32
Aurelien GALTIER <aurelien.galtier@live.fr>
2014-03-19 10:14:43 +0100

    Add unit test

---
+++ Dommy.Business.Test/Speech/SpeechToTextConfigTest.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Dommy.Business.Test.Speech
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Dommy.Business.Speech;
+
+    [TestClass]
+    public class SpeechToTextConfigTest : BaseTest
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            var kernel = this.CreateKernel();
+
+            var speechToTextConfig = new SpeechToTextConfig();
+
+            speechToTextConfig.Confidence = 0.6;
+            speechToTextConfig.Culture = "fr-FR";
+            speechToTextConfig.InfoSentenceFile = "c:\fileName.txt";
+
+            speechToTextConfig.Create(kernel);
+        }
+    }
+}
