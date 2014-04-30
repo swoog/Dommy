42016409231c2a9d9ef4e3abaaef6b6bca184890
Aurelien GALTIER <aurelien.galtier@live.fr>
2014-04-30 09:11:29 +0200

    Add xb86 console

---
+++ Dommy.Console.x86/Program.cs
@@ -0,0 +1,17 @@
+﻿using Dommy.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dommy.Console.x86
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bootstrap.Run();
+        }
+    }
+}
