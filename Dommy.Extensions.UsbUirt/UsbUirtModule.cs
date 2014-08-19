a47659bc48a70aafd9e9714bf0831645191892ad
Aurelien GALTIER <aurelien.galtier@live.fr>
2014-08-19 22:10:07 +0200

    Fix bugs

--- Dommy.Extensions.UsbUirt/UsbUirtModule.cs
+++ Dommy.Extensions.UsbUirt/UsbUirtModule.cs
@@ -23,6 +23,7 @@ namespace Dommy.Extensions.UsbUirt
             this.Kernel.Bind<IClientFactory<ITransmitter>>().To<ClientFactory<ITransmitter>>();
             this.Kernel.Bind<IClientFactory<IReceiver>>().To<ClientFactory<IReceiver>>();
 
+
             this.Kernel.Bind<UsbUirtListener>().ToSelf().InSingletonScope();
             this.Kernel.Bind<IListener>().ToMethod(c => c.Kernel.Get<UsbUirtListener>()).InSingletonScope();
             this.Kernel.Bind<IServiceHost>().ToMethod(c => new ServiceHost<UsbUirtListener>(c.Kernel.Get<UsbUirtListener>()));
--- Dommy.Web/App_Start/NinjectWebCommon.cs
+++ Dommy.Web/App_Start/NinjectWebCommon.cs
@@ -1,40 +1,28 @@
+//-----------------------------------------------------------------------
+// <copyright file="NinjectWebCommon.cs" company="TrollCorp">
+//     Copyright (c) agaltier, TrollCorp. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
 [assembly: WebActivator.PreApplicationStartMethod(typeof(Dommy.Web.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(Dommy.Web.App_Start.NinjectWebCommon), "Stop")]
 
 namespace Dommy.Web.App_Start
 {
-    using Dommy.Business;
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-    using Ninject;
-    using Ninject.Web.Common;
     using System;
     using System.Linq;
     using System.Web;
     using System.Web.Mvc;
+    using Dommy.Business;
+    using Dommy.Business.Services;
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Ninject;
+    using Ninject.Extensions.Conventions;
+    using Ninject.Web.Common;
 
     public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
-
-        /// <summary>
-        /// Starts the application
-        /// </summary>
-        public static void Start()
-        {
-            ViewEngines.Engines.Add(new ResolverEngine(ViewEngines.Engines.OfType<RazorViewEngine>().FirstOrDefault()));
-
-            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
-            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
-            bootstrapper.Initialize(CreateKernel);
-        }
-
-        /// <summary>
-        /// Stops the application.
-        /// </summary>
-        public static void Stop()
-        {
-            bootstrapper.ShutDown();
-        }
 
         /// <summary>
         /// Creates the kernel that will manage your application.
@@ -56,6 +44,28 @@ namespace Dommy.Web.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            kernel.Bind<IClientFactory<IEngine>>().To<ClientFactory<IEngine>>();
+            kernel.Bind<IClientFactory<ITileManager>>().To<ClientFactory<ITileManager>>();
+        }
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start()
+        {
+            ViewEngines.Engines.Add(new ResolverEngine(ViewEngines.Engines.OfType<RazorViewEngine>().FirstOrDefault()));
+
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
         }
     }
 }
