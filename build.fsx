29ed85bfa67658e5b1bbbddf39ab7c8a341c5436
Aurelien GALTIER <aurelien.galtier@live.fr>
2015-05-25 18:48:33 +0200

    Update deployement script

--- build.fsx
+++ build.fsx
@@ -4,9 +4,14 @@ open Fake
 
 // Properties
 let buildDir = "./build/"
+let apps =
+     ["Dommy.Console"
+      "Dommy.Console.x86"]
 let packages = 
      ["Dommy.Extensions.Kinect.Sdk1" 
-      "Dommy.Extensions.Kinect.Sdk2"]
+      "Dommy.Extensions.Kinect.Sdk2"
+      "Dommy.Extensions.UsbUirt"
+      "Dommy.Extensions.UsbUirt.x86"]
 
 // Clean target
 Target "Clean" (fun _ ->
@@ -19,9 +24,26 @@ Target "BuildApp" (fun _ ->
       |> Log "AppBuild-Output: "
 )
 
+Target "Apps" (fun _ ->
+    for app in apps do
+        !! ("./" + app + "/bin/release/*.*")
+        |> Copy (buildDir + "/apps/" + app) 
+)
+
 Target "Package" (fun _ ->
     for package in packages do
         !! ("./" + package + "/bin/release/*.*")
+        -- ("./" + package + "/bin/release/Dommy.Business.*")
+        -- ("./" + package + "/bin/release/Ninject.*")
+        -- ("./" + package + "/bin/release/CassiniDev4*")
+        -- ("./" + package + "/bin/release/Microsoft.AspNet*")
+        -- ("./" + package + "/bin/release/Microsoft.Owin*")
+        -- ("./" + package + "/bin/release/log4net*")
+        -- ("./" + package + "/bin/release/newtonsoft*")
+        -- ("./" + package + "/bin/release/roslyn*")
+        -- ("./" + package + "/bin/release/owin*")
+        -- ("./" + package + "/bin/release/castle*")
+        -- ("./" + package + "/bin/release/jetbrain*")
         |> Copy (buildDir + package) 
 )
 
@@ -32,6 +54,7 @@ Target "Default" (fun _ ->
 
 "Clean"
 ==> "BuildApp"
+==> "Apps"
 ==> "Package"
 ==> "Default"
 
